064f01ad431009d0bb7cbc6481e2e88774f33f24
luca abeni <luca.abeni@santannapisa.it>
2025-01-04 19:19:01 +0100

    multi: Add check in dl_server_update for initialized server
    A dl_server must be updated only if it has actually been initialized. This fixes a bug
    caused by switching SCHED_OTHER tasks (in a cgroup) to SCHED_FIFO, which causes the
    update of a possibly uninitialized dl_server.
    
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1680,6 +1680,8 @@ void dl_server_update_idle_time(struct rq *rq, struct task_struct *p)
 
 void dl_server_update(struct sched_dl_entity *dl_se, s64 delta_exec)
 {
+	if (!dl_server(dl_se)) return;
+
 	/* 0 runtime = fair server disabled */
 	if (dl_se->dl_runtime) {
 		dl_se->dl_server_idle = 0;
