095f97affc96f05bfcf2f829d1d4ec0d65ceba85
Yuri Andriaccio <yurand2000@gmail.com>
2025-01-14 11:34:12 +0100

    multi: Fix autogroups compilation also for multicpu

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2630,6 +2630,9 @@ static int tg_rt_schedulable(struct task_group *tg, void *data)
 	u64 period, runtime;
 	int cid;
 
+	if (task_group_is_autogroup(tg))
+		return 0;
+
 	cid = d->cid;
 	period = tg->dl_bandwidth.dl_period;
 
@@ -2684,6 +2687,9 @@ static int tg_rt_schedulable(struct task_group *tg, void *data)
 	 * The sum of our children's runtime should not exceed our own.
 	 */
 	list_for_each_entry_rcu(child, &tg->children, siblings) {
+		if (task_group_is_autogroup(child))
+			continue;
+
 		period  = child->dl_bandwidth.dl_period;
 		runtime = child->dl_se[cid]->dl_runtime;
 
