ac6f8bd348e59910e7c1f83e7eb7758ea8d59b4e
Yuri Andriaccio <yurand2000@gmail.com>
2025-04-30 10:32:12 +0200

    Remove support for cgroups v1
    Disable control files for cgroups v1, only cgroups v2 is allowed.
    Since the runtime allocated to rt-cgroups cannot be changed when using cgroups v1,
    it is now set by default to zero. The bandwidth is therefore reserved only for SCHED_DEADLINE tasks.
    
    Since the runtime is now zero by default, it must be allowed to turn on/off the
    root_control_group, i.e. setting its runtime to zero and completely disallow rt-cgroups.
    This specific check is removed in tg_rt_schedulable. Other changes affect the scheduling
    policy change and attaching FIFO/RR task to rt-cgroups.
    
    Disabling the root control group bandwidth should not cause any issue,
    as SCHED_FIFO/SCHED_RR tasks do not depend on it since the introduction of fair_servers.

--- kernel/sched/syscalls.c
+++ kernel/sched/syscalls.c
@@ -645,7 +645,7 @@ change:
 		 * assigned.
 		 */
 		if (dl_bandwidth_enabled() && rt_policy(policy) &&
-				task_group(p)->dl_bandwidth.dl_runtime == 0 &&
+				!sched_rt_can_attach(task_group(p), p) &&
 				!task_group_is_autogroup(task_group(p))) {
 			retval = -EPERM;
 			goto unlock;
