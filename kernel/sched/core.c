b1ac5a67335ccb14d4d6badf4fa7605f1c07dfce
luca abeni <luca.abeni@santannapisa.it>
2025-06-10 11:44:36 +0200

    sched/core: Initialize root_task_group
    Add the initialization function for task_group's dl_servers. Initialize the
    default bandwidth for rt-cgroups and the root control group. Add utility
    functions to check and get if a rt_rq entity is connected to a rt-cgroup.
    
    Co-developed-by: Alessio Balsini <a.balsini@sssup.it>
    Signed-off-by: Alessio Balsini <a.balsini@sssup.it>
    Co-developed-by: Andrea Parri <parri.andrea@gmail.com>
    Signed-off-by: Andrea Parri <parri.andrea@gmail.com>
    Co-developed-by: Yuri Andriaccio <yurand2000@gmail.com>
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8548,7 +8548,7 @@ void __init sched_init(void)
 		scx_tg_init(&root_task_group);
 #endif /* CONFIG_EXT_GROUP_SCHED */
 #ifdef CONFIG_RT_GROUP_SCHED
-		root_task_group.rt_se = (struct sched_rt_entity **)ptr;
+		root_task_group.dl_se = (struct sched_dl_entity **)ptr;
 		ptr += nr_cpu_ids * sizeof(void **);
 
 		root_task_group.rt_rq = (struct rt_rq **)ptr;
@@ -8560,6 +8560,11 @@ void __init sched_init(void)
 #ifdef CONFIG_SMP
 	init_defrootdomain();
 #endif
+
+#ifdef CONFIG_RT_GROUP_SCHED
+	init_dl_bandwidth(&root_task_group.dl_bandwidth,
+			global_rt_period(), global_rt_runtime());
+#endif /* CONFIG_RT_GROUP_SCHED */
 
 #ifdef CONFIG_CGROUP_SCHED
 	task_group_cache = KMEM_CACHE(task_group, 0);
@@ -8612,7 +8617,7 @@ void __init sched_init(void)
 		 * starts working after scheduler_running, which is not the case
 		 * yet.
 		 */
-		init_tg_rt_entry(&root_task_group, &rq->rt, NULL, i, NULL);
+		init_tg_rt_entry(&root_task_group, rq, NULL, i, NULL);
 #endif
 #ifdef CONFIG_SMP
 		rq->sd = NULL;
