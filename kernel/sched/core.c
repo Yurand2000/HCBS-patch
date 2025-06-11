f7b7f54620cfe32c2c05b5c33f803f60e5276a65
luca abeni <luca.abeni@santannapisa.it>
2025-06-11 11:27:17 +0200

    multi: Fix bug affecting kubernetes
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4182,6 +4182,7 @@ int try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 		 */
 		WRITE_ONCE(p->__state, TASK_WAKING);
 
+#if 0
 		/*
 		 * If the owning (remote) CPU is still in the middle of schedule() with
 		 * this task as prev, considering queueing p on the remote CPUs wake_list
@@ -4204,6 +4205,7 @@ int try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 		if (smp_load_acquire(&p->on_cpu) &&
 		    ttwu_queue_wakelist(p, task_cpu(p), wake_flags))
 			break;
+#endif
 
 		/*
 		 * If the owning (remote) CPU is still in the middle of schedule() with
