c466f7497a57951b72509f163eda2a78cfdf0b6f
Yuri Andriaccio <yurand2000@gmail.com>
2025-09-04 12:29:54 +0200

    [TODO] CPU hotplug code is still missing
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2278,6 +2278,7 @@ static void rq_online_rt(struct rq *rq)
 	if (rq->rt.overloaded)
 		rt_set_overload(rq);
 
+	/*FIXME: Enable the dl server! */
 	cpupri_set(&rq->rd->cpupri, rq->cpu, rq->rt.highest_prio.curr);
 }
 
@@ -2287,6 +2288,7 @@ static void rq_offline_rt(struct rq *rq)
 	if (rq->rt.overloaded)
 		rt_clear_overload(rq);
 
+	/* FIXME: Disable the dl server! */
 	cpupri_set(&rq->rd->cpupri, rq->cpu, CPUPRI_INVALID);
 }
 
