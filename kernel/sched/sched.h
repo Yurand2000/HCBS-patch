44b669b1353357d1196b4374bf4ab4276b9af295
Yuri Andriaccio <yurand2000@gmail.com>
2025-10-27 15:10:32 +0100

    sched/rt: Remove unnecessary runqueue pointer in struct rt_rq
    Remove the rq field in struct rt_rq.
      The rq field now is just caching the pointer to the global runqueue of the
      given rt_rq, so it is unnecessary as the global runqueue can be retrieved
      in other ways.
    
    Introduce served_rq_of_rt_rq to retrieve the runqueue the given rt_rq is
    serving.
    
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -857,8 +857,6 @@ struct rt_rq {
 	raw_spinlock_t		rt_runtime_lock;
 
 	unsigned int		rt_nr_boosted;
-
-	struct rq		*rq; /* this is always top-level rq, cache? */
 #endif
 #ifdef CONFIG_CGROUP_SCHED
 	struct task_group	*tg; /* this tg has "this" rt_rq on given CPU for runnable entities */
@@ -3335,11 +3333,16 @@ static inline struct task_struct *rt_task_of(struct sched_rt_entity *rt_se)
 	return container_of_const(rt_se, struct task_struct, rt);
 }
 
+static inline struct rq *served_rq_of_rt_rq(struct rt_rq *rt_rq)
+{
+	WARN_ON(!rt_group_sched_enabled() && rt_rq->tg != &root_task_group);
+	return container_of_const(rt_rq, struct rq, rt);
+}
+
 static inline struct rq *rq_of_rt_rq(struct rt_rq *rt_rq)
 {
 	/* Cannot fold with non-CONFIG_RT_GROUP_SCHED version, layout */
-	WARN_ON(!rt_group_sched_enabled() && rt_rq->tg != &root_task_group);
-	return rt_rq->rq;
+	return cpu_rq(served_rq_of_rt_rq(rt_rq)->cpu);
 }
 
 static inline struct rt_rq *rt_rq_of_se(struct sched_rt_entity *rt_se)
@@ -3351,6 +3354,11 @@ static inline struct rt_rq *rt_rq_of_se(struct sched_rt_entity *rt_se)
 static inline struct task_struct *rt_task_of(struct sched_rt_entity *rt_se)
 {
 	return container_of_const(rt_se, struct task_struct, rt);
+}
+
+static inline struct rq *served_rq_of_rt_rq(struct rt_rq *rt_rq)
+{
+	return container_of_const(rt_rq, struct rq, rt);
 }
 
 static inline struct rq *rq_of_rt_rq(struct rt_rq *rt_rq)
