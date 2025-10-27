f14e0da77c1240bf0e6354af25e2b51dc3d7d454
Yuri Andriaccio <yurand2000@gmail.com>
2025-10-27 10:15:22 +0100

    [FIXUP] Remove debug BUG_ONs in rt_queue_push/pull_task(s)

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -385,9 +385,6 @@ static inline void rt_queue_push_tasks(struct rt_rq *rt_rq)
 {
 	struct rq *rq = container_of(rt_rq, struct rq, rt);
 
-	BUG_ON(rt_rq == NULL);
-	BUG_ON(rq != cpu_rq(rq->cpu));
-
 	if (!has_pushable_tasks(rt_rq))
 		return;
 
@@ -397,9 +394,6 @@ static inline void rt_queue_push_tasks(struct rt_rq *rt_rq)
 static inline void rt_queue_pull_task(struct rt_rq *rt_rq)
 {
 	struct rq *rq = container_of(rt_rq, struct rq, rt);
-
-	BUG_ON(rt_rq == NULL);
-	BUG_ON(rq != cpu_rq(rq->cpu));
 
 	queue_balance_callback(rq, &per_cpu(rt_pull_head, rq->cpu), pull_rt_task);
 }
