0ec9fb779a95c3b72a3a95e55d9dd7a14fec87ae
Yuri Andriaccio <yurand2000@gmail.com>
2025-10-28 11:08:05 +0100

    sched/deadline: Introduce dl_server_try_pull_f

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -408,6 +408,7 @@ extern void dl_server_start(struct sched_dl_entity *dl_se);
 extern void dl_server_stop(struct sched_dl_entity *dl_se);
 extern void dl_server_init(struct sched_dl_entity *dl_se, struct dl_rq *dl_rq,
 		    struct rq *served_rq,
+		    dl_server_try_pull_f try_pull_task,
 		    dl_server_pick_f pick_task);
 extern void sched_init_dl_servers(void);
 extern int dl_check_tg(unsigned long total);
