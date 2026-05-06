2a3de2ba5a437b42a7a7455c6fbe0852cdc27358
Yuri Andriaccio <yurand2000@gmail.com>
2026-05-06 10:39:29 +0200

    [FIXUP] Remove set_next_task_rt in rt_server_pick

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -285,19 +285,16 @@ int alloc_rt_sched_group(struct task_group *tg, struct task_group *parent)
 }
 
 static struct sched_rt_entity *pick_next_rt_entity(struct rt_rq *rt_rq);
-static inline void set_next_task_rt(struct rq *rq, struct task_struct *p, bool first);
 
 static struct task_struct *rt_server_pick(struct sched_dl_entity *dl_se, struct rq_flags *rf)
 {
 	struct rt_rq *rt_rq = &dl_se->my_q->rt;
-	struct rq *rq = rq_of_rt_rq(rt_rq);
 	struct task_struct *p;
 
 	if (!sched_rt_runnable(dl_se->my_q))
 		return NULL;
 
 	p = rt_task_of(pick_next_rt_entity(rt_rq));
-	set_next_task_rt(rq, p, true);
 
 	return p;
 }
