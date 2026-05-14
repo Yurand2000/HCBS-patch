02b291425851ba17f26355226316dd78ca728d90
Yuri Andriaccio <yurand2000@gmail.com>
2026-05-14 14:16:54 +0200

    [FIXUP] __checkparam_dl

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -3705,7 +3705,7 @@ bool __checkparam_dl(const struct sched_attr *attr, bool allow_zero_runtime)
 		return true;
 
 	/* deadline != 0 */
-	if (attr->sched_deadline == 0)
+	if (!allow_zero_runtime && attr->sched_deadline == 0)
 		return false;
 
 	/*
@@ -3736,7 +3736,7 @@ bool __checkparam_dl(const struct sched_attr *attr, bool allow_zero_runtime)
 	max = (u64)READ_ONCE(sysctl_sched_dl_period_max) * NSEC_PER_USEC;
 	min = (u64)READ_ONCE(sysctl_sched_dl_period_min) * NSEC_PER_USEC;
 
-	if (period < min || period > max)
+	if ((!allow_zero_runtime || period != 0) && (period < min || period > max))
 		return false;
 
 	return true;
