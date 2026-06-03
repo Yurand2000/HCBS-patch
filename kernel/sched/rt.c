45b808506684c22fffc45b24071256fd0b0ddecd
Yuri Andriaccio <yurand2000@gmail.com>
2026-06-03 15:46:33 +0200

    [FIXUP] Remove unnecessary IS_ENABLED(CONFIG_RT_GROUP_SCHED)

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -972,8 +972,7 @@ select_task_rq_rt(struct task_struct *p, int cpu, int flags)
 	bool test;
 
 	/* Just return the task_cpu for processes inside task groups */
-	if (IS_ENABLED(CONFIG_RT_GROUP_SCHED) &&
-	    is_dl_group(rt_rq_of_se(&p->rt)))
+	if (is_dl_group(rt_rq_of_se(&p->rt)))
 		goto out;
 
 	/* For anything but wake ups, just return the task_cpu */
@@ -1246,7 +1245,7 @@ static void put_prev_task_rt(struct rq *rq, struct task_struct *p, struct task_s
 	if (on_rt_rq(&p->rt) && p->nr_cpus_allowed > 1)
 		enqueue_pushable_task(rt_rq, p);
 
-	if (IS_ENABLED(CONFIG_RT_GROUP_SCHED) && is_dl_group(rt_rq)) {
+	if (is_dl_group(rt_rq)) {
 		struct sched_dl_entity *dl_se = dl_group_of(rt_rq);
 
 		if (dl_se->dl_throttled)
@@ -2053,7 +2052,7 @@ static void switched_to_rt(struct rq *rq, struct task_struct *p)
 	if (task_current(rq, p)) {
 		update_rt_rq_load_avg(rq_clock_pelt(rq), rq, 0);
 
-		if (IS_ENABLED(CONFIG_RT_GROUP_SCHED) && is_dl_group(rt_rq)) {
+		if (is_dl_group(rt_rq)) {
 			struct sched_dl_entity *dl_se = dl_group_of(rt_rq);
 
 			p->dl_server = dl_se;
