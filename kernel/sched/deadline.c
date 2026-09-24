7e9eda5673f248b5c20a54dd79eb8188ee061f63
Yuri Andriaccio <yurand2000@gmail.com>
2026-09-24 14:54:43 +0200

    [FIXUP] Fix sub_nr_running to correctly include rt dl_servers.
    Sashiko AI review: https://sashiko.dev/#/patchset/20260608121546.69910-1-yurand2000@gmail.com?part=14
    
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2303,7 +2303,7 @@ void dec_dl_tasks(struct sched_dl_entity *dl_se, struct dl_rq *dl_rq)
 		sub_nr_running(rq_of_dl_rq(dl_rq), 1);
 	else if (rq_of_dl_se(dl_se) != dl_se->my_q) {
 		WARN_ON(dl_se->my_q->rt.rt_nr_running != dl_se->my_q->nr_running);
-		sub_nr_running(rq_of_dl_rq(dl_rq), dl_se->my_q->nr_running - 1);
+		sub_nr_running(rq_of_dl_rq(dl_rq), dl_se->my_q->nr_running + 1);
 	}
 
 	dec_dl_deadline(dl_rq, dl_se->deadline);
